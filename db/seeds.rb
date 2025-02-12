7140baf7b57c76766c33afb0d1f230543617d25f
Dominique <dominiquebertisch@gmail.com>
2025-02-12 16:16:13 +0100

    1243155

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,22 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+# 2. Create the instances 🏗️
+puts "Creating restaurants..."
+Restaurant.create!(name: "Dishoom", address: "London", phone_number: "012345", category: "belgian")
+puts "Created Dishoom"
+Restaurant.create!(name: "Pizza East", address: "London", phone_number: "987654321", category: "italian")
+puts "Created Pizza East"
+Restaurant.create!(name: "Alex' Bistro", address: "Berlin", phone_number: "987654321", category: "french")
+puts "Created Alex Bistro"
+Restaurant.create!(name: "Doms' Bistro", address: "Berlin", phone_number: "7654321", category: "japanese")
+puts "Created Doms Bistro"
+Restaurant.create!(name: "Ming dynasie", address: "Berlin", phone_number: "454545454", category: "chinese")
+puts "Created Ming dynasie"
+
+# 3. Display a message 🎉
+puts "Finished! Created #{Restaurant.count} restaurants."
